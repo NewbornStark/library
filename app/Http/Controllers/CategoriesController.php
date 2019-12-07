b9b8359a851f2705a2dc43f524cc56896601dc4a
Ricardo Sandoval <ricorsd@gmail.com>
2019-12-06 20:40:46 -0600

    editar y eliminar categorias

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\BookCategories;
 use App\Category;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,51 @@ class CategoriesController extends Controller
     {
         $categories = Category::all();
         return view('categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        $category = new Category;
+        return view('categories.form', compact('category'));
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'name' => 'required|max:50'
+        ]);
+        
+        if (!$category = Category::find(request('id'))) {
+            Category::create([
+                'name' => request('name')
+            ]);
+        } else {
+            $category->name = request('name');
+            $category->save();
+        }
+
+        return redirect()->route('categories');
+    }
+
+    public function edit(int $id = null)
+    {
+        $category = Category::findOrFail($id);
+        return view('categories.form', compact('category'));
+    }
+
+    public function isRelatedToBook(int $id = null)
+    {
+        
+        return intval(BookCategories::where('id_category', $id)->exists());
+    }
+
+    public function delete(int $id = null)
+    {
+        // Se borran las relaciones de libros con la categoria a eliminar
+        $relatedBooks = BookCategories::where('id_category', $id)->delete();
+        // Se borra la categoria
+        Category::destroy($id);
+        $message = 'La categoria se eliminó correctamente.';
+        return $message;
     }
 }
